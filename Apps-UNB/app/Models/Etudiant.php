0b17cf5338c422e505023fd8d8e67f2822f602dc
adouabou <mradouabou97@gmail.com>
2022-06-20 22:57:26 +0200

    ajoutbouton

---
+++ Apps-UNB/app/Models/Etudiant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant extends Model
+{
+    use HasFactory;
+    protected $table = 'etudiants';
+    protected $fillable = [
+        'nom',
+         'prenom',
+         'email',
+         'matricule',
+         'cycle',
+         'niveau_etude',
+         'annee_academique',
+         'photo',
+
+    ];
+}
--- Apps-UNB/resources/views/home.blade.php
+++ Apps-UNB/resources/views/home.blade.php
@@ -12,24 +12,73 @@
                             <div class="card-body">
                             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Inscrire un étudiant</button>
                             <button type="button" class="btn btn-success">Listes des étudiants</button>
-                            <button type="button" class="btn btn-success">Nommer une sécrétaire</button>
+                            
+                            <button type="button" class="btn btn-success">Gestion sécretaire</button>
                             </div>
                             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <div class="modal-header" style="background-color: #FFC300;">
+                                        <h5 class="modal-title"  style="color:#050705  ;" id="exampleModalLabel">Enregister un étudiant</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
+                                    <form>
                                     <div class="modal-body">
-                                        ...
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <label for="inputName" class="form-label">Nom(s)</label>
+                                                <input required type="text" name="nom" class="form-control" id="inputName">
+                                            </div>
+                                             <div class="col-6">
+                                                <label for="inputName" class="form-label">Prénom</label>
+                                                <input required type="text" name="nom" class="form-control" id="inputName">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-6">
+                                            <label for="inputName" class="form-label">Cycle</label>
+                                             <select class="form-select">
+                                               
+                                               <option value="">Licence</option>
+                                             </select>
+                                            </div>
+                                            <div class="col-6">
+                                            <label for="inputName" class="form-label">Niveau d'etude</label>
+                                             <select class="form-select">
+                                               <option value="">Licence 1</option>
+                                               <option value="">Licence 2</option>
+                                               <option value="">Licence 3</option>
+                                             </select>
+                                            </div>
+                                        </div>
+                                            <div class="row">
+                                            <div class="col-6">
+                                                <label for="inputName" class="form-label">Anneé academique</label>
+                                                <input required type="text" name="nom" class="form-control" id="inputName">
+                                            </div>
+                                             <div class="col-6">
+                                                <label for="inputName" class="form-label">Matricule</label>
+                                                <input required type="text" name="nom" class="form-control" id="inputName">           
+                                        </div>
+                                         </div>
+                                            <div class="row">
+                                            <div class="col-6">
+                                                <label for="inputName" class="form-label">Email</label>
+                                                <input required type="text" name="nom" class="form-control" id="inputName">
+                                            </div>
+                                             <div class="col-6">
+                                                <label for="inputName" class="form-label">Photo</label>
+                                                <input required type="file" name="nom" class="form-control" id="inputName">           
+                                        </div>
+
                                     </div>
                                     <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                                        <button type="button" class="btn btn-primary">Enregistrer</button>
                                     </div>
+                                    </form>
                                     </div>
                                 </div>
                                 </div>
@@ -42,13 +91,13 @@
                             <div class="card-body">
                              <table class="table align-middle mb-5 bg-white">
                                     <thead class="bg-light">
-                                        <tr>
+                                        <tr >
                                         <th>IDENTITE</th>
                                         <th>Matricule</th>
                                         <th>Cycle</th>
                                         <th>Niveau d'étude</th>
                                         <th>Année Academique</th>
-                                        <th>Actions</th>
+                                        <th style="text-align :center;">Actions</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -81,12 +130,19 @@
                                            <td>
                                             <p class="fw-normal mb-1">2021-2022</p>
                                         </td>
-                                        <td>
-                                        @can('admin')
-                                            <button type="button" class="btn btn-info btn-sm btn-rounded">
-                                            Voir
+                                        <td style="text-align :center;">
+                                        {{-- @can('admin') --}}
+                                            <button type="button" class="btn btn-info btn-sm btn-rounded serviceeditbtn">
+                                            Modifier
                                             </button>
-                                        @endcan
+                                             <button type="button" class="btn btn-danger btn-sm btn-rounded">
+                                            Supprimer
+                                            </button>
+                                             <button type="button" class="btn btn-info btn-sm btn-rounded">
+                                            Carte
+                                            </button>
+                                            
+                                        {{-- @endcan --}}
                                         </td>
                                         </tr>
                                     </tbody>
@@ -102,6 +158,16 @@
     </div>
     
 </div>
+<script>
+       $(document).ready(function(){
+        $(`.serviceeditbtn`).click(function (e){
+            e.preventDefault();
+            alert('hello');
+
+        })
+
+       })
+</script>
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
